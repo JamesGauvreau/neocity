40d5597bba0a20502758f9c731c75eb8afe22a98
JamesGauvreau <james.longfrenchname@gmail.com>
2023-07-09 19:28:15 -0700

    images - archives - scluetiful

---
+++ public/projects/generators.js
@@ -0,0 +1,30 @@
+const minceFat = [
+    `olives`,
+    `tallow`, 
+    `marrow`, // obtained by boiling bones
+]
+
+const minceProtein = [
+    `beans`,
+    `cheese`,
+    `duck`,
+    `fish`, // especially oily fish
+    `insect meal`,
+    `nuts`,
+    `oat and pea flour`,
+]
+
+const minceFruit = [
+    `cherries`,
+    `chili peppers`, //high in vitamin c
+    `mushrooms`, // umami flavor
+    `rose hips`,
+    `strawberries`,
+]
+
+const minceOther = [
+    `eggs`,
+    `thyme`, //high in vitamin c
+]
+
+// small intestines eaten like noodles
