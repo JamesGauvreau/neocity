a6e5e3bafc82b8adbb2f6a21fb1f52151ca1e0ff
JamesGauvreau <james.longfrenchname@gmail.com>
2023-07-12 23:18:31 -0700

    index page

---
+++ public/projects/famgen.js
@@ -0,0 +1,41 @@
+//  ::  Overview
+
+//  ::   Family section
+// Lifespan: Determines the average magical lifespan. -- Editable, with default of 140. 
+// Age of Family Head: Determines how family head's age is rolled. Dropdown list including `Mature` [Lifespan - 1d20+10 years], `Established` [Lifespan / 2 + 1d10 years], `Adult` [Lifespan / 5 + 1d10 years], `Young` [ ]
+// Fertility Window: Age range. 
+// Marriage age: [Editable number; default 16] + [ed. num.; default 1] to [ed. num.; default 6]
+    // Error checking to ensure that ed. num 1 isn't greater than ed. num 2? Else, check which is lowest and use that one.     
+// Fecundity: The range rolled for each individual to determine their maximum children. [ed. num.; default 1] to [ed. num.; default 6].
+// Sibling spacing: The number of years added to each parent's age before rolling another child. [ed. num; default 1] to [ed. num; default 4].
+// Family Trait: -- Dropdown list including `None`, `Random (Trait Assured)`, and `Random (Possibly No Trait)`.
+// Family House: -- Dropdown list including `Gryffindor`, `Hufflepuff`, `Ravenclaw`, `Slytherin`, and `None`. 
+    //  Later, add capacity to select multiple options. 
+    //  Later, add `Random Family House` option. 
+// House Lean: 
+    // The weighting of a Family House, when rolling for an individual's House. 
+// Family Culture: -- Dropdown list including `English`, `Irish`, `Scottish`, `Welsh`. 
+    //  Later, add `Cornish`, `Other`, and maybe additional cultures. 
+// Namelist: An optional dropdown list. If not selected, names default to Family Culture.
+    //  Capacity to mix multiple namelists with checkboxes?
+    // Ability to upload your own namelists? 
+
+//  ::  Individuals 
+    //  Begin with one individual, the family head.
+    //  Give names and ages of parents, siblings, children. 
+    //  Capacity to edit fields after generation? If so, there should be an error-checking function somewhere.
+    //  Capacity for queer wixes. 
+    //  Capacity for outliers (checkbox to dis/allow?)
+// Name: 
+// Age: 
+// Gender: 
+// Married at age: 
+// House: 
+// Maximum children: Hidden value. Once this number is reached, the generator stops producing children. 
+
+//  ::  Broader things to work on
+    //  Capacity to randomize all input values. 
+    //  Capacity to customize the majority of fields. 
+    //  Capacity to download info, either as-is or as code.
+    //  Capacity to upload info in order to restore a saved family.
+    //  Something to combine families? This might be the trickiest part. 
