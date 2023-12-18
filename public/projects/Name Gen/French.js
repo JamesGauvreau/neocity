d4ec6a56130398effb9ea71ef30136147f2317fe
JamesGauvreau <james.longfrenchname@gmail.com>
2023-12-17 18:46:55 -0800

    french name gen - basic js framework

---
+++ public/projects/Name Gen/French.js
@@ -0,0 +1,8 @@
+//  ::  Names
+
+let firstName = `John`;
+let lastName = `Smith`;
+
+document.getElementById(`generator`).innerHTML = `${firstName} ${lastName}`;
+
+https://stackoverflow.com/questions/54708626/random-name-generator-works
