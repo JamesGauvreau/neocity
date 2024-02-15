97d43d4ac84338c49b9971e255823016c9686263
JamesGauvreau <james.longfrenchname@gmail.com>
2024-02-14 17:42:43 -0800

    footer and header work

---
+++ public/projects/EPC/footer.js
@@ -0,0 +1,11 @@
+const legalFoosterJS = document.getElementById("legalFooterJS");
+const footerElement = document.createElement("footer");
+footerElement.innerHTML = `
+        <p> Copyright © 2024 Emergency Power Corporation 
+        &emsp;|&emsp;
+        All Rights Reserved 
+        &emsp;|&emsp;
+        <a href="contactus.html">Contact Us</a>
+        </p>`;
+
+legalFooterJS.append(footerElement);
---
+++ public/projects/EPC/header.js
@@ -0,0 +1,12 @@
+const contactHeaderJS = document.getElementById("contactHeaderJS");
+const headerElement = document.createElement("header");
+headerElement.innerHTML = `
+<div class="capsule" style="min-width: 50px;">
+When Being Online Counts
+</div>
+<div class="capsule-right" style="min-width: 200px" ;>
+Contact Us&emsp;|&emsp;(313) 535 - 1434
+</div>
+    `;
+
+contactHeaderJS.append(headerElement);
