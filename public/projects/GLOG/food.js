0c2df969357b665b38b1d01ef3e41bd970566cf5
JamesGauvreau <james.longfrenchname@gmail.com>
2024-11-09 20:56:53 -0800

    notes for street food

---
+++ public/projects/GLOG/food.js
@@ -0,0 +1,36 @@
+// fish in every variety, sweetmeats and sweatbreads, eighty-eight types of
+// sandwiches
+
+const food = [
+    "baked potatoes", 
+    "buttered pea pods",
+    "currant buns",
+    "meat puddings",
+    "boiled eggs",
+    "sheep's trotters",
+]
+
+const seafood = [
+    "eel jelly",
+    "jellied eel",
+    "pickled whelks",
+    "oysters in the shell",
+    "fish in every variety",
+]
+
+const beverage = [
+    "hot wine",
+    "herb beer",
+    "lemonade",
+    "ginger ale",
+    "coffee",
+    "chocolate tea",
+    "dogstone tea",
+    "real tea",
+    "new milk from the cow",
+    "ass milk, fresh from the ass",
+]
+
+// Rations: for longer excursions - berries; fish skin chips; dark rye bread,
+// soaked with beer; crusty sourdough bread, dried anchovies; smoked fish, rye
+// crackers
