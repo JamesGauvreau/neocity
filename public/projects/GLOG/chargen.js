8f20ff3efcb70130a6f8b6be64f6832e3d802190
JamesGauvreau <james.longfrenchname@gmail.com>
2024-11-24 18:15:26 -0800

    chargen

---
+++ public/projects/GLOG/chargen.js
@@ -0,0 +1,24 @@
+// KIND
+// Roll 1d20: 1 each per Bugbear, Packling, or Snerson. Rest are human: 50% chance of mutant: 50% chance of non-normal mutant: 50% chance of 1 mutation, else 2 mutations. 
+
+// CALLING
+// Roll 1d8: If wizard, roll 1d8 again.
+
+// CLASS
+// Roll as normal.
+
+// ABILITY SCORES
+// Roll each, then check against Kind and re-roll, take highest, of one score.
+
+// DERIVED SCORES
+// As normal.
+// Pick Grit from highest non-Flesh score.
+
+// SKILLS
+// Roll randomly based on Calling.
+// Remove lastmost skills if necessary.
+
+// EQUIPMENT
+// Roll starting funds
+// Roll random kit
+// Add penny knife
