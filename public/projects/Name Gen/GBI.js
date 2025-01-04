908c9bc0646ddb4035a024e9a5f9776379cb4526
JamesGauvreau <james.longfrenchname@gmail.com>
2025-01-04 00:30:10 -0800

    minor edits, setting up js pages for name generators

---
+++ public/projects/Name Gen/GBI.js
@@ -0,0 +1,18 @@
+// Great Britain and Ireland
+
+// SCHEME
+// Each name consists of:
+// 0    Formal / Latin
+// 1    Diminutive - may have up to two forms
+// 2    Standard Name
+// 3    Spelling Variant of Standard Name
+// 4    Alternate Version
+// 5    Empty
+// 6    Empty
+// 7    Empty
+// 8    Empty
+// 9    Empty
+// 10   Language-specific group: If there is no language-specific variant for a
+//      given language, then it contains the standard name
+
+//
---
+++ public/projects/Name Gen/WizBritFamilies.js
@@ -0,0 +1,16 @@
+// TODO
+// All names
+// Consider whether to break up further, e.g. Pure-blood, Wizengamot seat, etc. 
+
+const familyCornish = [
+    "Dearborn",
+    "Gagwilde",
+    "Lovegood",
+    "Nettles",
+    "Penrose",
+    "Sawley",
+    "Trelawney",
+    "Spallyer",
+    "Higgs",
+    "Rosewall",
+]
