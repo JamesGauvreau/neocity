0ecc82ab1af4420ef9280f054c80b13d88e71d3d
JamesGauvreau <james.longfrenchname@gmail.com>
2025-01-10 23:19:46 -0800

    glog, doom wizard city, names, sneople

---
+++ public/projects/GLOG/namegen.js
@@ -0,0 +1,33 @@
+const sneopleBasqueMasc = [
+  "Alessander", // Defender of men
+  "Esstebe", // Crow, wreath; that which surrounds
+  "Jake", // Holder of the heel, supplanter
+  "Frantzissko", // Frenchman
+  "Jassone", // Lift up; to raise
+  "Josseba", // He will add
+  "Eguzski", // Sun
+  "Gaizska", // Savior
+  "Oinatz", // Footprint
+  "Zsigor", // Rod, staff
+  "Zsorion", // Happiness
+  "Salazsar", // 
+];
+
+const sneopleBasqueFem = [
+    "Alessandra",
+    "Esskarne",
+    "Essti",
+    "Frantzisska",
+    "Ossane",
+    "Jossebe",
+    "Ssorne",
+    "Eguzska",
+    "Teresse",
+    "Udane",
+    "Ssua",
+    "Zsorione",
+]
+
+const gravesburyMasc = [
+    
+]
