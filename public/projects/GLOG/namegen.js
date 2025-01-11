5ac8f4eb137082779e8cb7e6b47b923c781b7d81
JamesGauvreau <james.longfrenchname@gmail.com>
2025-01-10 23:29:20 -0800

    gravesbury names

--- public/projects/GLOG/namegen.js
+++ public/projects/GLOG/namegen.js
@@ -10,24 +10,100 @@ const sneopleBasqueMasc = [
   "Oinatz", // Footprint
   "Zsigor", // Rod, staff
   "Zsorion", // Happiness
-  "Salazsar", // 
+  "Salazsar", //
 ];
 
 const sneopleBasqueFem = [
-    "Alessandra",
-    "Esskarne",
-    "Essti",
-    "Frantzisska",
-    "Ossane",
-    "Jossebe",
-    "Ssorne",
-    "Eguzska",
-    "Teresse",
-    "Udane",
-    "Ssua",
-    "Zsorione",
-]
+  "Alessandra",
+  "Esskarne",
+  "Essti",
+  "Frantzisska",
+  "Ossane",
+  "Jossebe",
+  "Ssorne",
+  "Eguzska",
+  "Teresse",
+  "Udane",
+  "Ssua",
+  "Zsorione",
+];
 
-const gravesburyMasc = [
-    
-]
+const gravesMasc = [
+  // 12 names
+  "Hesso",
+  "Wolfram",
+  "Wolschalk",
+  "Ralph (or Rawe)",
+  "Grendel",
+  "Adam",
+  "Hezel",
+  "Geoffrey (or Gallfridus)",
+  "Madulf",
+  "Sisulf",
+  "Sinbald",
+  "Lautrude",
+];
+
+const gravesFem = [
+  // 12 names
+  "Hildesende",
+  "Hersent",
+  "Samanilde",
+  "Wulftrude",
+  "Engelsent",
+  "Sunilda",
+  "Hellsinde",
+  "Hessa",
+  "Hildeswith",
+  "Mortimeria",
+  "Desdemona",
+  "Burnswith",
+];
+
+const gravesSurname = [
+  // 24 names
+  "Blackburn",
+  "Bloodgood",
+  "Boddis",
+  "Bonehill",
+  "Brisbane",
+  "Chill",
+  "Cutbirth",
+  "Darkwater",
+  "Deft",
+  "Giblett",
+  "Goodlife",
+  "Graves",
+  "Marylebone",
+  "Offal",
+  "Organer",
+  "Shank",
+  "Skellington",
+  "Smallbone",
+  "Sollas",
+  "Talloe (Tallow)",
+  "Tooth",
+  "Truebody",
+  "Wakeman",
+  "Winteur (Winter)",
+];
+
+const gravesPrenomNumber = 12;
+const gravesPostnomNumber = 24;
+
+function randomNumber(number) {
+    return Math.floor(Math.random() * number);
+  };
+
+function gravesNameDigger() {
+    prenomNumber = randomNumber(gravesPrenomNumber);
+    postnomNumber = randomNumber(gravesPostnomNumber);
+    genderNumber = randomNumber(2);
+    if (genderNumber === 0) {
+        return gravesMasc[prenomNumber] + " " + gravesSurname[postnomNumber];
+    } else {
+        return gravesFem[prenomNumber] + " " + gravesSurname[postnomNumber];
+    }
+};
+
+console.log(gravesNameDigger());
