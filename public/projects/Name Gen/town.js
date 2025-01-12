039720a597ac4e48df042b5a822bede5293fd73f
JamesGauvreau <james.longfrenchname@gmail.com>
2025-01-11 22:07:38 -0800

    notes added

--- public/projects/Name Gen/town.js
+++ public/projects/Name Gen/town.js
@@ -1,15 +1,28 @@
 const suffix = [
-    "ay", // OE, "island", e.g. Ramsay, Westray; also -y, -ey
-    "bury", // OE, "fortified enclosure"
-    "borough", // OE, as -bury
-    "brough", // OE, as -bury
-    "burgh", // OE, as -bury
-    "by", // ON, "settlement, village"
-    "cester", // OE, "camp"
-    "ford", // OE, "shallow place in a river that is easily crossed"
-    "ham", // OE, "farm, homestead"
-    "mouth", // "mouth of a river"
-    "stead", // OE, "place, enclosed pasture"
-    "ton", // OE, "enclosure, estate"
-    "worth", // OE, "enclosure"
-]
+  "ay", // OE, "island", e.g. Ramsay, Westray; also -y, -ey
+  "bury", // OE, "fortified enclosure"
+  "borough", // OE, as -bury
+  "brough", // OE, as -bury
+  "burgh", // OE, as -bury
+  "by", // ON, "settlement, village"
+  "cester", // OE, "camp"
+  "ford", // OE, "shallow place in a river that is easily crossed"
+  "ham", // OE, "farm, homestead"
+  "mouth", // "mouth of a river"
+  "stead", // OE, "place, enclosed pasture"
+  "ton", // OE, "enclosure, estate"
+  "worth", // OE, "enclosure"
+];
+
+// Wiltshire -- 
+// -font, -ham, =brock, -burn, 
+// Goatacre, lit. goat-acre. 
+// Great X. West X. 
+// Grimstead = grēnehāmstede , 'green homestead.' 
+// The first part of the name of the village may be derived from the Old English for 'gate by the ford'. Millicent comes from Millicent de Clinton, who owned the manor in the 12th century and was the wife of William de Clinton;[2][3] the suffix distinguishes the manor from its neighbour to the south, Lydiard Tregoze.
+
+// OMINOUS
+// Ravens, Hunger, Gore, Crow, Withern, Barrow
+// Barrow's Green, Blackwater, Blackwell in the Peak, Black Dog, Witchampton, Hexton, Gravesend, Barrow Haven, Adderbury, Ashwell, Wednesfield
+
+// -den, Green, -field, -ampton, -sea, -well, -whistle, -field
