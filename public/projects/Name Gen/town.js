021a362985cc705dd35d52063c2a2ec25dbc7a7d
JamesGauvreau <james.longfrenchname@gmail.com>
2025-01-11 21:34:10 -0800

    town name, suffixes

---
+++ public/projects/Name Gen/town.js
@@ -0,0 +1,15 @@
+const suffix = [
+    "ay", // OE, "island", e.g. Ramsay, Westray; also -y, -ey
+    "bury", // OE, "fortified enclosure"
+    "borough", // OE, as -bury
+    "brough", // OE, as -bury
+    "burgh", // OE, as -bury
+    "by", // ON, "settlement, village"
+    "cester", // OE, "camp"
+    "ford", // OE, "shallow place in a river that is easily crossed"
+    "ham", // OE, "farm, homestead"
+    "mouth", // "mouth of a river"
+    "stead", // OE, "place, enclosed pasture"
+    "ton", // OE, "enclosure, estate"
+    "worth", // OE, "enclosure"
+]
