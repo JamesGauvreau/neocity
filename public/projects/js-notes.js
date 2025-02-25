e37b415b9597faf410014077600bb514568f10bb
cmSalticidae <james.longfrenchname@gmail.com>
2025-02-24 23:20:44 -0800

    added notes

--- public/projects/js-notes.js
+++ public/projects/js-notes.js
@@ -18,26 +18,28 @@ const dog = bottlecapper("green bottle", "white cap")
 
 // Besides primitives (e.g. strings, booleans), there are objects.
 // Objects may be: 
-    // 1. standard objects
-    // 2. arrays
-    // 3. functions
-        // This means that 'function bottlecapper' is an object.
+// 1. standard objects
+// 2. arrays
+// 3. functions
+// This means that 'function bottlecapper' is an object.
 
 // defining the function
 
 console.log(dog)
 console.log(bottlecapper("green bottle", "white cap"))
-// ^ the preceding are equivalent 
+// ^ the preceding are equivalent
 
 // machine machineName(inputs_ie_rawMaterial) {
 //      output_ie_product
-// 
+//
 // function bottlecapper(bottle, cap) {
 //     return bottle + cap;
 // }
 
 // In document.querySelector, "document" represents the entire HTML document. If querySelector looks for CSS, you need to precede with # (i.e. #Button1).
 
-// If you try to reassign a const, JS will throw an error. 
+// If you try to reassign a const, JS will throw an error.
 
 // "You have repetition in the goTown and goStore functions. Repetition in your code is a sign that you need another function."
+
+// // Objects are non primitive data types that store key-value pairs. Non primitive data types are mutable data types that are not undefined, null, boolean, number, string, or symbol. Mutable means that the data can be changed after it is created.
