0bfc4bc348b0b31dc0f7289c5a397f10941a6235
cmSalticidae <james.longfrenchname@gmail.com>
2025-04-06 16:57:14 -0700

    cleaned up comments

--- public/projects/GLOG/innovations.js
+++ public/projects/GLOG/innovations.js
@@ -45,39 +45,6 @@ const innovations = [
 
 console.log(innovations[0].title)
 
-// const innoMMM = {
-//     title: "Miles' Moving Miracles",
-//     value: 1,
-// };
-// const innoRoom = {
-//     title: "Room to Live",
-//     value: 1,
-// };
-// const innoWithoutRoads = {
-//     title: "A World Without Roads",
-//     value: 1,
-// };
-// const innoPoly = {
-//     title: "True Polymorph",
-//     value: 1,
-// };
-// const innoPeaceful = {
-//     title: "A Peaceful City",
-//     value: 1,
-// };
-// const innoWorkforce = {
-//     title: "Conjured Workforce",
-//     value: 1,
-// };
-// const innoIron = {
-//     title: "Coal & Iron",
-//     value: 1,
-// };
-// const innoCreation = {
-//     title: "The Power of Creation",
-//     value: 1,
-// };
-
 function roll1d(number) {
     return Math.floor(Math.random() * number) + 1;
 }
@@ -95,14 +62,6 @@ function rollForInnovation() {
         let rolls = [] // ← build something that can keep track of the previous rolls. 
         // let rolls = [roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6)];
         let rollsBad = [roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6), roll1d(6)];
-        // let r1a = roll1d(6);  // Roll a 6-sided die
-        // let r2a = roll1d(6);
-        // let r3a = roll1d(6);
-        // let r4a = roll1d(6);
-        // let r5a = roll1d(6);
-        // let r6a = roll1d(6);
-        // let r7a = roll1d(6);
-        // let r8a = roll1d(6);
 
         // If the roll is greater than innoMMM.value, increment the value
         for (let i = 0; i < innovations.length; i++) {
@@ -137,15 +96,4 @@ function rollForInnovation() {
     return result.join('');
 }
 
-
-
-
 console.log(rollForInnovation());
-// console.log(randomNumber(6));
-
-// Pseudo
-// Function must roll 1d6 for each innovation. If result > current value, increment value by 1.
-// After each innovation is rolled, increment yearCurrent by 1.
-// If any value is 6, end.
-// Else, increment yearCurrent by 1 and start again.
-// Output each result, with \n between years. 
