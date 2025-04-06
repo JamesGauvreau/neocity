ede24d5ba357ce737fa0382389bd7a48093a4c8a
cmSalticidae <james.longfrenchname@gmail.com>
2025-04-06 02:58:54 -0700

    tech journal update, tinkering with the namegen.

--- public/projects/Name Gen/British.js
+++ public/projects/Name Gen/British.js
@@ -13,6 +13,24 @@ function randomNumber(number) {
 // Use checkboxes to define isMasc, isFem, etc., and populate 'let genderArray' as appropriate.
 // Use checkboxes to define cultures, and pop the appropriate cultures into 'let cultureArray'.
 // Use genderArray.length and cultureArray.length for randomNumber().
+// if result is undefined, start at beginning and try again. 
+// should check whether there was already an issue, and if there's an issue more than three times, instead return "N/A (recurring error)"
+// For isFlower, etc., use a loop to check each item and pop into a new array if the criteria are fit.
+
+let testArray = portArray[0].engFem;
+let isFlower = false;
+
+function testPush() {
+    if (isFlower === true) {
+        testArray.push("Flower name");
+    } else {
+        // dog = false;
+    }
+    return testArray;
+}
+
+console.log("testArray test: " + portArray[0].engFem[0]);
+console.log("testPush test: " + testPush());
 
 function prenomGen() {
     let prenomArray = portArray[randomNumber(portArray.length)].engFem[0];
@@ -21,3 +39,4 @@ function prenomGen() {
 
 console.log(prenomGen());
 console.log(portArray.length);
+console.log(portArray[0].engFem[0]);
