ff006184f4e1f6c6195e8ec425603a19458578ec
cmSalticidae <james.longfrenchname@gmail.com>
2025-07-23 00:12:32 -0700

    revising portfolio page

--- public/index.js
+++ public/index.js
@@ -2,13 +2,15 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
+    if (panel.style.maxHeight && panel.classList.contains("open")) {
       panel.style.maxHeight = null;
+      panel.classList.remove("open");
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.classList.add("open");
     }
   });
 }
